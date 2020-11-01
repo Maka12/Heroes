7f2db93c19906bcd8fea777cb24fcf9afe5b57fc
Orlando Dos Santos <68990926+orlandoDosSantos@users.noreply.github.com>
2020-11-01 09:51:46 +0000

    template components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeroesReadComponent } from './heroes/heroes-read/heroes-read.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesReadComponent
+    HeroesReadComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
